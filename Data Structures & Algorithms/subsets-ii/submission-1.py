7fd3b8d01750a20e42c99ba54a49a582f4f8b8f3
Morris Kuang <chuanwei.0925@gmail.com>
2026-09-21 01:20:59 -0400

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+        decision tree - sort first [1,1,2]
+        [1] []
+        [1,1] [], [1] []
+        [1,1,2] [2], [1,2] [2] 
+        if same, skip
+
+        base - last element
+        """
+        
+        ans, sol = [], []
+        nums.sort()
+        n = len(nums)
+
+        def traverse(layer):
+            if sol not in ans:
+                ans.append(sol.copy())
+            
+            if layer == n:  # if it goes first, it doesnt allow the last sol to be appended
+                return
+
+            # decision 1
+            sol.append(nums[layer])
+            traverse(layer+1)
+            sol.pop()
+
+            # decision 2
+            traverse(layer+1)
+
+
+        
+        traverse(0)
+        return ans
