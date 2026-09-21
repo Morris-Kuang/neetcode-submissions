a99e0bdfc28953f4151403f0dcd0a50802a63eec
Morris Kuang <chuanwei.0925@gmail.com>
2026-09-21 02:17:20 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,46 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        decision tree - first put 1 "(", decide between a "(" or a ")"
+        "("
+        "((" , "()" 
+        "(((" "(()" , "()(" "())*" invalid - open < close
+        *invalid - open > n
+        
+        base: if len(sol) == 2n
+        """
+
+        ans, sol = [], []
+        opn = 1
+        close = 0
+        sol.append("(") # as a starter
+
+        def backtrack():
+            nonlocal opn
+            nonlocal close
+            
+            if opn < close:
+                return
+            if opn > n or close > n:
+                return
+            if len(sol) == 2 * n:
+                ans.append("".join(sol.copy()))
+            
+            # decision 1 - "("
+            sol.append("(")
+            opn += 1
+            backtrack()
+            sol.pop()
+            opn -= 1
+
+            #decision 2 - ")"
+            sol.append(")")
+            close += 1
+            backtrack()
+            sol.pop()
+            close -= 1
+
+        
+        backtrack()
+        return ans
+
